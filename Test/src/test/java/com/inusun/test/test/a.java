ecff66d2acf41e9a75833803eef2feff29bb575d
SJY <17839167062@163.com>
2022-11-09 09:16:50 +0800

    test

--- Test/src/test/java/com/inusun/test/test/a.java
+++ Test/src/test/java/com/inusun/test/test/a.java
@@ -54,6 +54,8 @@ public class a {
             }
             System.out.println(str);
         } catch (IOException e) {
+            // TODO
+            System.out.println("");
         }
     }
 }
